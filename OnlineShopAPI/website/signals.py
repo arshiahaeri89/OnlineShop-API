c6736e0c7d45b4cda630041879f0d5446b65e8a9
Arshia-Haeri-Mehrizi <haeri.arshia@gmail.com>
2025-03-23 15:26:19 +0330

    improve the signal logic

--- OnlineShopAPI/website/signals.py
+++ OnlineShopAPI/website/signals.py
@@ -6,5 +6,9 @@ from .models import ProductImage
 
 @receiver(post_delete, sender=ProductImage)
 def delete_image_file(sender, instance, **kwargs):
-    if instance.image and os.path.isfile(instance.image.path):
-        os.remove(instance.image.path)  # Note: Change This For Cloud Storages
+    folder_path = os.path.dirname(instance.image.path)
+    if instance.image and instance.image.name and instance.image.storage.exists(instance.image.name):
+        instance.image.delete(save=False)
+
+    if os.path.isdir(folder_path) and not os.listdir(folder_path):
+        os.rmdir(folder_path)
